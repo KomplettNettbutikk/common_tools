903afc7260e461c9341bbce28aa60b2da4015731
Mads Oliver Jenssen <30602638+madsoliver@users.noreply.github.com>
2023-04-18 13:32:25 +0200

    Update db_backup.sh

--- db_backup.sh
+++ db_backup.sh
@@ -12,7 +12,7 @@
 # Note: Running scripts directly from the internet can pose a security risk. Make sure to verify the script content before executing it.
 
 # Set backup directory path
-BACKUP_DIR="/db-backups"
+BACKUP_DIR="/db_backups"
 
 # Create backup directory if it doesn't exist
 if [ ! -d "$BACKUP_DIR" ]; then
@@ -25,14 +25,14 @@ if [ -n "$1" ]; then
   BACKUP_RETENTION_SECONDS=$(($1 * 3600))
 
   # Delete backups older than retention time
-  FIND_COMMAND="find \"$BACKUP_DIR\" -name \"db-backup*.sql\" -type f -mmin \"+$(($1 * 60))\" -delete"
+  FIND_COMMAND="find \"$BACKUP_DIR\" -name \"db_backup*.sql\" -type f -mmin \"+$(($1 * 60))\" -delete"
 else
   # No retention time provided
   FIND_COMMAND=""
 fi
 
 # Create backup filename with current timestamp
-BACKUP_FILENAME="$BACKUP_DIR/db-backup-$(date +%Y-%m-%d_%H-%M-%S).sql"
+BACKUP_FILENAME="$BACKUP_DIR/db_backup-$(date +%Y-%m-%d_%H-%M-%S).sql"
 
 # Take database backup
 mysqldump --all-databases > "$BACKUP_FILENAME"
