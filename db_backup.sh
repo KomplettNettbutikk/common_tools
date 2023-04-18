55cf4d122b44b0765baf3167d3138f016f287aab
Mads Oliver Jenssen <30602638+madsoliver@users.noreply.github.com>
2023-04-18 13:28:46 +0200

    Update db_backup.sh

--- db_backup.sh
+++ db_backup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-## ##
+# Backup script - creates backups of all databases and deletes backups older than a specified time
+#
+# Usage:
+# 1. Download the script from https://raw.githubusercontent.com/KomplettNettbutikk/common_tools/main/db_backup.sh
+# 2. Make the script executable: chmod +x db_backup.sh
+# 3. Add the script as a cron job using the following one-liner (replace "/path/to/db_backup.sh" with the actual path to the script):
+#    (curl -sS https://raw.githubusercontent.com/KomplettNettbutikk/common_tools/main/db_backup.sh > /db_backup.sh && chmod +x /db_backup.sh && crontab -l ; echo "0 * * * * /db_backup.sh 24") | crontab -
+#    This will execute the script every hour and retain backups for 24 hours. Replace "24" with the desired retention time in hours.
+#
+# Note: Running scripts directly from the internet can pose a security risk. Make sure to verify the script content before executing it.
 
 # Set backup directory path
 BACKUP_DIR="/db-backups"
@@ -10,29 +19,25 @@ if [ ! -d "$BACKUP_DIR" ]; then
   mkdir "$BACKUP_DIR"
 fi
 
-# Set backup interval and retention time in hours with default values
-BACKUP_INTERVAL_HOURS="${1:-1}"
-BACKUP_RETENTION_HOURS="${2:-24}"
-
-# Calculate backup interval in seconds
-BACKUP_INTERVAL_SECONDS=$(($BACKUP_INTERVAL_HOURS * 3600))
-
-# Calculate backup retention time in seconds
-BACKUP_RETENTION_SECONDS=$(($BACKUP_RETENTION_HOURS * 3600))
-
-# Loop forever
-while true; do
-
-  # Create backup filename with current timestamp
-  BACKUP_FILENAME="$BACKUP_DIR/db-backup-$(date +%Y-%m-%d_%H-%M-%S).sql"
-
-  # Take database backup
-  mysqldump --all-databases > "$BACKUP_FILENAME"
+# Check if retention time is provided
+if [ -n "$1" ]; then
+  # Calculate backup retention time in seconds
+  BACKUP_RETENTION_SECONDS=$(($1 * 3600))
 
   # Delete backups older than retention time
-  find "$BACKUP_DIR" -name "db-backup*.sql" -type f -mmin "+$BACKUP_RETENTION_MINUTES" -delete
+  FIND_COMMAND="find \"$BACKUP_DIR\" -name \"db-backup*.sql\" -type f -mmin \"+$(($1 * 60))\" -delete"
+else
+  # No retention time provided
+  FIND_COMMAND=""
+fi
 
-  # Sleep until next backup interval
-  sleep "$BACKUP_INTERVAL_SECONDS"
+# Create backup filename with current timestamp
+BACKUP_FILENAME="$BACKUP_DIR/db-backup-$(date +%Y-%m-%d_%H-%M-%S).sql"
 
-done
+# Take database backup
+mysqldump --all-databases > "$BACKUP_FILENAME"
+
+# Delete backups older than retention time (if set)
+if [ -n "$FIND_COMMAND" ]; then
+  eval "$FIND_COMMAND"
+fi
